a54d53b6a10acc02d455dc86c0f45b2d77f7e838
misiaq <pawel.kwiecien@o2.pl>
2013-03-28 20:44:57 +0100

    message.java

---
+++ Patronage-task-2/src/main/java/task2/model/Message.java
@@ -0,0 +1,16 @@
+package task2.model;
+
+public class Message {
+
+String name;
+	
+	String message;
+	public String getMessage() {
+		return name;
+	}
+	public void setMessage(String name) {
+		this.name = name;
+	}
+	public Message() {
+	} 
+}
